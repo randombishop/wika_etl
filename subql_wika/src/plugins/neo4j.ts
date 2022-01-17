cf27bdd966861ceb260927a96cce363b7ab28b4a
randombishop <nabil.abdellaoui@expr-research.com>
2022-01-17 10:04:46 -0500

    neo4j plugin

--- subql_wika/src/plugins/neo4j.ts
+++ subql_wika/src/plugins/neo4j.ts
@@ -26,8 +26,132 @@ export class PluginNeo4j {
         return (this.isEnabled==1) ;
     }
 
-    async handleLikeEvent(): Promise<void>{
+    async fetchUrlNode(url: string) {
+        const result = await this.session.run(
+            'MATCH (a:Url {url: $url}) RETURN a',
+            { url: url }
+        )
+        if (result.records.length==1) {
+            const singleRecord = result.records[0]
+            const node = singleRecord.get(0)
+            return node ;
+        } else {
+            return null ;
+        }
+    }
 
+    async createUrlNode(url: string, numLikes: number) {
+        const result = await this.session.run(
+            'CREATE (a:Url {url: $url, numLikes: $numLikes}) RETURN a',
+            {url: url, numLikes: numLikes}
+        )
+        const singleRecord = result.records[0]
+        const node = singleRecord.get(0)
+        return node ;
+    }
+
+    async updateUrlNode(url: string, numLikes: number) {
+        await this.session.run(
+            'MATCH (a:Url {url: $url}) SET a.numLikes=a.numLikes+$numLikes',
+            {url: url, numLikes: numLikes}
+        )
+    }
+
+    async fetchUserNode(address: string) {
+        const result = await this.session.run(
+            'MATCH (a:User {address: $address}) RETURN a',
+            {address: address}
+        )
+        if (result.records.length==1) {
+            const singleRecord = result.records[0]
+            const node = singleRecord.get(0)
+            return node ;
+        } else {
+            return null ;
+        }
+    }
+
+    async createUserNode(address: string, numLikes: number) {
+        const result = await this.session.run(
+            'CREATE (a:User {address: $address, numLikes: $numLikes}) RETURN a',
+            {address: address, numLikes: numLikes}
+        )
+        const singleRecord = result.records[0]
+        const node = singleRecord.get(0)
+        return node ;
+    }
+
+    async updateUserNode(address: string, numLikes: number) {
+        await this.session.run(
+            'MATCH (a:User {address: $address}) SET a.numLikes=a.numLikes+$numLikes',
+            {address: address, numLikes: numLikes}
+        )
+    }
+
+    async fetchRelation(url: string, address: string) {
+        const result = await this.session.run(
+            'MATCH (a:User {address: $address})-[r:LIKES]->(a:Url {url: $url}) RETURN r',
+            {url: url, address: address}
+        )
+        if (result.records.length==1) {
+            const singleRecord = result.records[0]
+            const relation = singleRecord.get(0)
+            return relation ;
+        } else {
+            return null ;
+        }
+    }
+
+    async createRelation(address: string, url: string, numLikes: number) {
+        let cql = "MATCH (a:User {address: $address}) " ;
+        cql += "MATCH (b:Url {url: $url}) " ;
+        cql += "CREATE (a)-[r:LIKES {numLikes: $numLikes}]->(b) RETURN r" ;
+        const result = await this.session.run(cql,
+            {address: address, url: url, numLikes: numLikes}
+        )
+        const singleRecord = result.records[0]
+        const node = singleRecord.get(0)
+        return node ;
+    }
+
+    async updateRelation(address: string, url: string, numLikes: number) {
+        let cql = "MATCH (a:User {address: $address})-[r:LIKES {numLikes: $numLikes}]->(b:Url {url: $url}) " ;
+        cql+= "SET r.numLikes = r.numLikes + $numLikes"
+        await this.session.run(
+            'MATCH (a:User {address: $address}) SET a.numLikes=a.numLikes+$numLikes',
+            {address: address, url: url, numLikes: numLikes}
+        )
+    }
+
+    async handleLikeEvent(url: string, user: string, numLikes: number): Promise<void>{
+        logger.debug('handleLikeEvent', url, user, numLikes) ;
+
+        // Create or update Url
+        let urlNode = await this.fetchUrlNode(url) ;
+        if (urlNode==null) {
+            urlNode = await this.createUrlNode(url, numLikes) ;
+        } else {
+            await this.updateUrlNode(url, numLikes) ;
+        }
+        logger.debug('urlNode', urlNode) ;
+
+        // Create or update User
+        let userNode = await this.fetchUserNode(user) ;
+        if (userNode==null) {
+            userNode = await this.createUserNode(user, numLikes) ;
+        } else {
+            await this.updateUserNode(user, numLikes) ;
+        }
+        logger.debug('userNode', userNode) ;
+
+        // Create or update the relationship
+        let relation = await this.fetchRelation(url, user) ;
+        if (relation==null) {
+            relation = await this.createRelation(user, url, numLikes) ;
+        } else {
+            await this.updateRelation(user, url, numLikes) ;
+        }
+        logger.debug('relation', relation) ;
     }
 
     async dispose(): Promise<void>{
